a45d3176d40af18bc0fc5287cf31f7c043b7e38c
bernborgess <bernborgess@outlook.com>
2023-10-23 20:14:17 -0300

    feat: Aceitar uso de JSON

--- api/src/index.ts
+++ api/src/index.ts
@@ -4,13 +4,8 @@ import { router } from "./routes";
 
 const app = express();
 
+app.use(express.json());
 app.use(morgan("dev"));
 app.use(router);
-
-app.get("/tasks", (req, res) => {
-    res.json({})
-        .status(200);
-})
-
 
 export { app };
