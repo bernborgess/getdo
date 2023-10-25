51e35af5c66428258f5572c536e61ca7c474af21
bernborgess <bernborgess@outlook.com>
2023-10-25 15:02:05 -0300

    feat: deleting tasks

--- api/src/contracts/Repository.ts
+++ api/src/contracts/Repository.ts
@@ -3,4 +3,5 @@ import { Task } from "../models/task";
 export interface Repository {
     tasks(): Promise<Task[]>;
     createTask(data: { title: string; day: number; }): Promise<Task>;
+    deleteTask(id: string): Promise<void>;
 }
