79da1a3d7a76a695430cb57e21983594dce20ad7
bernborgess <bernborgess@outlook.com>
2023-11-01 15:59:40 -0300

    feat: cors working with backend

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,6 +1,6 @@
 import { app } from ".";
 
-app.listen(3000, () =>
-    console.log("API is running on 3000")
+app.listen(process.env.PORT, () =>
+    console.log(`API is running on ${process.env.PORT}`)
 );
 
--- client/src/pages/Home/index.tsx
+++ client/src/pages/Home/index.tsx
@@ -1,8 +1,29 @@
+import { api } from "../../services/api";
+
 export function Home() {
 
+  async function apiCall() {
+    const res = await api.get("tasks");
+    console.log(res.data)
+  }
+
   return (
-    <div>
-      Home
-    </div >
+    <div
+      style={{
+        flex: 1,
+        width: "100%",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center"
+      }}
+    >
+
+      <div>
+        <h1> Home </h1>
+      </div >
+      <button onClick={apiCall}>
+        GET BACK!
+      </button>
+    </div>
   )
 }
