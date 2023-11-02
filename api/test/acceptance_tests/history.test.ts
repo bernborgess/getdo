47f29ff11cf972e6a8ccb10f9b1c2b86bb86adba
bernborgess <bernborgess@outlook.com>
2023-11-02 16:56:56 -0300

    feat: history acceptance test

--- api/test/acceptance_tests/history.test.ts
+++ api/test/acceptance_tests/history.test.ts
@@ -1,5 +1,7 @@
 import * as request from "supertest";
 import { app } from "../../src";
+import { History } from "../../src/models/history";
+import { prismaClient as db } from "../../src/resources/PrismaClient";
 
 let server;
 
@@ -12,6 +14,26 @@ describe("GET /history", () => {
         const res = await request(app).get("/history");
         expect(res.status).toBe(200);
     });
+
+    it("SHOULD return a list of 2 histories WHEN db contains 2 histories", async () => {
+        const histories = [
+            new History("Cook rice and beans", new Date("2023-11-02T07:24:00")),
+            new History("Take the trash out", new Date("2023-11-03T07:24:00")),
+        ];
+
+        await db.history.deleteMany();
+        await db.history.create({ data: histories[0] });
+        await db.history.create({ data: histories[1] });
+
+        const res = await request(app).get("/history");
+
+        const resHistories: History[] = res.body.map(
+            hist => ({ title: hist.title, finishedAt: new Date(hist.finishedAt) })
+        );
+
+        expect(resHistories).toEqual(histories);
+
+    })
 
 
     afterAll(() => {
